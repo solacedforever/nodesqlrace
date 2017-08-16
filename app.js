e0d75339701421540ce23cd43510fcc786f59b86
James Spargo <solacedforever@gmail.com>
2017-08-16 13:07:25 -0400

    able to add runner and view all runners now

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
 const express = require('express');
 const db = require('./db');
-const bodyParser = require('body-parser')
-const mustacheExpress= require('mustache-express');
-let app = express();
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
-app.use(express.static('public'));
 app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/', (req, res, next) => {
   db.query('SELECT * FROM runner', [], (err, results) => {
     if (err) {
       return next(err)
     }
-    res.render('index', { results:results.rows})
-  })
-})
+    res.render('index', {runner:results.rows})
+  });
+});
 
-app.get('/a_runner/:results', (req, res, next) => {
-  db.query('SELECT * FROM runner', [0], (err, results) => {
+app.get('/add_runner', (req,res) => {
+  res.render('add_runner')
+});
+
+app.post('/add_runner', (req, res, next) => {
+  let addRunner =
+  `INSERT INTO runner(division,sponsor,name)
+  VALUES('${req.body.division}','${req.body.sponsor}','${req.body.name}')`;
+  db.query(addRunner, (err) =>{
+    if (err){
+      return next(err)
+    }
+    res.redirect('/')
+  });
+});
+
+app.get('/:id', (req, res, next) => {
+  const id = req.params.id
+  db.query(`SELECT * FROM runner WHERE bib_id = ${id}`, (err, results) => {
     if (err) {
       return next(err)
     }
-    res.render('a_runner',{results:results.rows})
-  })
-})
+    res.render('a_runner', {runner:results.rows})
+  });
+});
 
-
-// app.get('/:bibid', (req, res, next) => {
-//   db.query('SELECT * FROM bib_id', [], (err, results) => {
-//     if (err) {
-//       return next(err)
-//     }
-//     res.send(results.rows[0])
-//   })
-// })
-//
-// app.get('/:bibid', (req, res, next) => {
-//   db.query('INSERT * FROM bib_id', [], (err, results) => {
-//     if (err) {
-//       return next(err)
-//     }
-//     res.send(results.rows[0])
-//   })
-// })
-
-app.listen(3000,  () => {
-  console.log('Server listening on port 3000...');
+app.listen(3000, () => {
+console.log('Server listening on port 3000...');
 });
